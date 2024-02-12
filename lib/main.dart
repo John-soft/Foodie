999dfb39281b506d6ded739930911ea22d886743
John-soft <akinwalere52@gmail.com>
2024-02-12 22:25:08 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:foodie/constants/constants.dart';
+import 'package:foodie/views/entrypoint.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+        designSize: const Size(375, 825),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (_, child) {
+          return GetMaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Foodie App',
+            theme: ThemeData(
+              scaffoldBackgroundColor: kOffWhite,
+              iconTheme: const IconThemeData(color: kDark),
+              primarySwatch: Colors.grey,
+            ),
+            home: const MainScreen(),
+          );
+        });
+  }
+}
